dfd57449dd01e958104bbcd7c8e34a4763180a2f
orzechdev <tarasiukdawid@gmail.com>
2019-03-08 12:33:27 +0100

    Init repo

---
+++ gen_algorithm.py
@@ -0,0 +1,24 @@
+def start():
+    print('Start')
+    population = initialise(population_size=50)
+
+
+def initialise(population_size):
+    pass
+
+
+def evaluate():
+    pass
+
+
+def selection():
+    pass
+
+
+def crossover():
+    pass
+
+
+def mutation():
+    pass
+
---
+++ main.py
@@ -0,0 +1,11 @@
+from gen_algorithm import start
+
+
+def main():
+    print('Genetic Algorithms starts')
+    start()
+    print('Genetic Algorithms end')
+
+
+main()
+
