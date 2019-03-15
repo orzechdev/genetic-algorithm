f2b1500b50e356e572085dd4140de1db4c6a2837
orzechdev <tarasiukdawid@gmail.com>
2019-03-15 21:20:21 +0100

    Rebuild init and evaluate functions and add plottiing graph

--- gen_algorithm.py
+++ gen_algorithm.py
@@ -1,49 +1,76 @@
 import numpy as np
-from utils import print_title
+import matplotlib.pyplot as plt
+from utils import print_title, plot_polynomial
 
 
-def start(points):
+def start(negative_points, positive_points):
     print_title('GA: start - polynomial degree 2')
     print_title('GA: initialise population of functions arguments')
-    args_population = initialise(args_population_size=50)
+    args_population = initialise(args_population_size=100)
     print(args_population)
     print_title('GA: evaluate negative and positive points groups')
-    args_population_evaluated = evaluate(args_population, points)
-    print(args_population_evaluated)
+    args_population_fittnes_values = evaluate(args_population, negative_points, positive_points)
+    print(args_population_fittnes_values)
 
 
 def initialise(args_population_size):
     # Initialize the initial population of parameters of functions
     # [[a1,a2,a3,...,an],[b1,b2,b3,...,bn]]
-    return np.random.randint(10, size=(2, args_population_size))
+    # 1 <= a <= 10
+    # 0 <= b <= -50
+    # 0 <= c <= 100
+    a_params = np.random.randint(low=1, high=5, size=args_population_size)
+    b_params = np.random.randint(low=-100, high=0, size=args_population_size)
+    c_params = np.random.randint(low=0, high=1000, size=args_population_size)
+    # return np.random.randint(low=1, high=10, size=(2, args_population_size))
+    return np.concatenate(([a_params], [b_params], [c_params]))
 
 
-def evaluate(args_population, points):
+def evaluate(args_population, negative_points, positive_points):
     # Evaluate to which group (negative or positive) assign each point
     # [[[x1,y1],[x2,y2],...,[xn,yn]],[[x3,y3],[x4,y4],...,[xn,yn]]]
-    args_population_evaluated = []
+    # args_population_evaluated = []
 
-    args_population_negative = np.empty((0, 2))
-    args_population_positive = np.empty((0, 2))
+    # args_population_negative = np.empty((0, 2))
+    # args_population_positive = np.empty((0, 2))
+    # negative_points_fittnes_values = np.empty((1, 2))
+    # positive_points_fittnes_values = np.empty((1, 2))
+    args_population_fittnes_values = np.empty((0, 2))
 
     for n in range(0, args_population.shape[1]):
         a = args_population[0, n]
         b = args_population[1, n]
-        for i in range(0, points.shape[1]):
-            y_polynomial = a*(points[0, i]**2) + b*(points[0, i])
-            if y_polynomial > points[1, i]:
-                args_population_negative = np.concatenate((args_population_negative, [[points[0, i], points[1, i]]]))
-            else:
-                args_population_positive = np.concatenate((args_population_positive, [[points[0, i], points[1, i]]]))
-            # x_arr = np.roots(a, b, -points[1, i])
-        print('Negative points: ' + f'{args_population_negative.shape[0]}')
-        print('Positive points: ' + f'{args_population_positive.shape[0]}')
-        args_population_evaluated.append([args_population_negative, args_population_positive])
-        args_population_negative = np.empty((0, 2))
-        args_population_positive = np.empty((0, 2))
+        c = args_population[2, n]
+        plot_polynomial(a, b, c)
+        fittnes_value = 0
+        for i in range(0, negative_points.shape[1]):
+            y_polynomial = (a * (negative_points[0, i] ** 2)) + (b * (negative_points[0, i])) + c
+            if y_polynomial > negative_points[1, i]:
+                fittnes_value += 1
+        for i in range(0, positive_points.shape[1]):
+            y_polynomial = (a * (positive_points[0, i] ** 2)) + (b * (positive_points[0, i])) + c
+            if y_polynomial < positive_points[1, i]:
+                fittnes_value += 1
+        args_population_fittnes_values = np.append(args_population_fittnes_values, fittnes_value)
+
+    # for n in range(0, args_population.shape[1]):
+    #     a = args_population[0, n]
+    #     b = args_population[1, n]
+    #     for i in range(0, points.shape[1]):
+    #         y_polynomial = a*(points[0, i]**2) + b*(points[0, i])
+    #         if y_polynomial > points[1, i]:
+    #             args_population_negative = np.concatenate((args_population_negative, [[points[0, i], points[1, i]]]))
+    #         else:
+    #             args_population_positive = np.concatenate((args_population_positive, [[points[0, i], points[1, i]]]))
+    #         # x_arr = np.roots(a, b, -points[1, i])
+    #     print('Negative points: ' + f'{args_population_negative.shape[0]}')
+    #     print('Positive points: ' + f'{args_population_positive.shape[0]}')
+    #     args_population_evaluated.append([args_population_negative, args_population_positive])
+    #     args_population_negative = np.empty((0, 2))
+    #     args_population_positive = np.empty((0, 2))
 
     # args_population_evaluated = np.concatenate((args_population_negative, args_population_positive), axis=1)
-    return args_population_evaluated
+    return args_population_fittnes_values
 
 
 def selection():
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pickle
+import matplotlib.pyplot as plt
 from gen_algorithm import start
 from utils import print_title
 
@@ -7,22 +8,44 @@ from utils import print_title
 def main():
     print_title('Genetic Algorithm starts')
     # generate_and_save_points()
-    points = get_points()
-    print_title('Points')
-    print(points)
+    negative_points = get_negative_points()
+    positive_points = get_positive_points()
+    print_title('Negative and positive points')
+    print(negative_points)
+    print(positive_points)
+    plt.plot(negative_points[0], negative_points[1], 'r.')
+    plt.plot(positive_points[0], positive_points[1], 'b.')
+    plt.axis([0, 100, 0, 100])
 
-    start(points)
+    start(negative_points, positive_points)
+    plt.show()
     print_title('Genetic Algorithms end')
 
 
 def generate_and_save_points():
-    random_points = np.random.randint(100, size=(2, 1000))
-    with open('points', 'wb') as fp:
-        pickle.dump(random_points, fp)
+    # random_points = np.random.randint(100, size=(2, 1000))
+    random_points_negative_x = np.random.normal(70, 10, 500)
+    random_points_negative_y = np.random.normal(20, 10, 500)
+    random_points_positive_x = np.random.normal(20, 10, 500)
+    random_points_positive_y = np.random.normal(80, 10, 500)
+
+    random_points_negative = np.concatenate(([random_points_negative_x], [random_points_negative_y]))
+    random_points_positive = np.concatenate(([random_points_positive_x], [random_points_positive_y]))
+
+    with open('random_points_negative', 'wb') as fp:
+        pickle.dump(random_points_negative, fp)
+
+    with open('random_points_positive', 'wb') as fp:
+        pickle.dump(random_points_positive, fp)
 
 
-def get_points():
-    with open('points', 'rb') as fp:
+def get_negative_points():
+    with open('random_points_negative', 'rb') as fp:
+        return pickle.load(fp)
+
+
+def get_positive_points():
+    with open('random_points_positive', 'rb') as fp:
         return pickle.load(fp)
 
 
