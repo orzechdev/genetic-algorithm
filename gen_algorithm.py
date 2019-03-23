66950e36b6d0da0cdec54137e20b4170cd9b4088
orzechdev <tarasiukdawid@gmail.com>
2019-03-24 00:23:02 +0100

    Support multiple degree functions

--- gen_algorithm.py
+++ gen_algorithm.py
@@ -3,82 +3,65 @@ import matplotlib.pyplot as plt
 from utils import print_title, plot_polynomial, random_one, unpack_bits, mutate_bits
 
 
-def start(negative_points, positive_points):
-    param_population_size = 1000  # 2000
-    param_crossover_probability = 0.4  # 0.4
-    param_mutation_probability = 0.12  # 0.02
-    param_generation_number = 20  # 20
-
+def start(negative_points, positive_points, y_polynomial, initial_pop, param_crossover_probability, param_mutation_probability, param_generation_number):
     print_title('GA: start - polynomial degree 2')
     print_title('GA: initialise population of functions arguments')
-    args_population = initialise(args_population_size=param_population_size)
-    print(args_population)
+    print(initial_pop)
     print_title('GA: evaluate negative and positive points groups')
-    args_population_fitness_values = evaluate(args_population, negative_points, positive_points)
-    print(args_population_fitness_values)
+    args_pop_fitness_values = evaluate(initial_pop, negative_points, positive_points, y_polynomial)
+    print(args_pop_fitness_values)
     print_title('GA: selection, sort population by fitness values')
-    args_population_selected, best_value, worst_value, mean_value = selection_sort(args_population, args_population_fitness_values)
+    args_pop_selected, best_value, worst_value, mean_value = selection_sort(initial_pop, args_pop_fitness_values)
 
-    print(args_population_selected)
+    print(args_pop_selected)
     print('%.2f\t%.2f\t%.2f' % (best_value.astype(float), mean_value.astype(float), worst_value.astype(float)), end='\n')
 
     for i in range(param_generation_number - 1):
-        args_population_crossovered = crossover(args_population, args_population_selected, param_crossover_probability)
-        args_population_mutated = mutation(args_population_crossovered, param_mutation_probability)
-        args_population_fitness_values = evaluate(args_population_mutated, negative_points, positive_points)
-        args_population_selected, best_value, worst_value, mean_value = selection_sort(args_population_mutated, args_population_fitness_values)
+        args_pop_crossovered = crossover(initial_pop, args_pop_selected, param_crossover_probability)
+        args_pop_mutated = mutation(args_pop_crossovered, param_mutation_probability)
+        args_pop_fitness_values = evaluate(args_pop_mutated, negative_points, positive_points, y_polynomial)
+        args_pop_selected, best_value, worst_value, mean_value = selection_sort(args_pop_mutated, args_pop_fitness_values)
         print('%.2f\t%.2f\t%.2f' % (best_value.astype(float), mean_value.astype(float), worst_value.astype(float)), end='\n')
 
-    a = args_population_selected[0, 0]
-    b = args_population_selected[1, 0]
-    c = args_population_selected[2, 0]
-    plot_polynomial(a, b, c)
+    args = args_pop_selected[:, 0][::-1]
+    x = np.linspace(-10, 20, 1000)
+    y = y_polynomial(x, *args)
+    plt.plot(x, y)
 
     print('\n')
-    print(args_population_selected)
+    print(args_pop_selected)
 
 
-def initialise(args_population_size):
-    # Initialize the initial population of parameters of functions
-    # [[a1,a2,a3,...,an],[b1,b2,b3,...,bn]]
-    # 1 <= a <= 10
-    # 0 <= b <= -50
-    # 0 <= c <= 100
-    a_params = np.random.uniform(low=0.01, high=1, size=args_population_size)
-    b_params = np.random.randint(low=-20, high=20, size=args_population_size)
-    c_params = np.random.randint(low=-200, high=200, size=args_population_size)
-    # return np.random.randint(low=1, high=10, size=(2, args_population_size))
-    return np.concatenate(([a_params], [b_params], [c_params]))
+def evaluate(args_pop, negative_points, positive_points, y_polynomial):
 
+    args_pop_fitness_values = np.empty((0, 2))
 
-def evaluate(args_population, negative_points, positive_points):
-
-    args_population_fitness_values = np.empty((0, 2))
-
-    for n in range(0, args_population.shape[1]):
-        a = args_population[0, n]
-        b = args_population[1, n]
-        c = args_population[2, n]
+    for n in range(0, args_pop.shape[1]):
+        args = args_pop[:, n][::-1]
         # plot_polynomial(a, b, c)
         fitness_value = 0
         for i in range(0, negative_points.shape[1]):
-            y_polynomial = (a * (negative_points[0, i] ** 2)) + (b * (negative_points[0, i])) + c
-            if y_polynomial > negative_points[1, i]:
+            y_value = y_polynomial(negative_points[0, i], *args)
+            # y_polynomial = (a * (negative_points[0, i] ** 2)) + (b * (negative_points[0, i])) + c
+            if y_value > negative_points[1, i]:
                 fitness_value += 1
         for i in range(0, positive_points.shape[1]):
-            y_polynomial = (a * (positive_points[0, i] ** 2)) + (b * (positive_points[0, i])) + c
-            if y_polynomial < positive_points[1, i]:
+            y_value = y_polynomial(positive_points[0, i], *args)
+            # y_polynomial = (a * (positive_points[0, i] ** 2)) + (b * (positive_points[0, i])) + c
+            if y_value < positive_points[1, i]:
                 fitness_value += 1
-        args_population_fitness_values = np.append(args_population_fitness_values, fitness_value)
+        args_pop_fitness_values = np.append(args_pop_fitness_values, fitness_value)
 
-    # args_population_evaluated = np.concatenate((args_population_negative, args_population_positive), axis=1)
-    return args_population_fitness_values
+    # args_pop_evaluated = np.concatenate((args_pop_negative, args_pop_positive), axis=1)
+    return args_pop_fitness_values
 
 
 def selection_sort(args_pop, args_pop_fitness_values):
     args_pop_fitness_values_selected = np.empty((0, 2))
 
-    args_pop_selected = np.zeros((3, args_pop.shape[1]), dtype=np.float16)
+    args_number = args_pop.shape[0]
+
+    args_pop_selected = np.zeros((args_number, args_pop.shape[1]), dtype=np.float16)
     args_pop_fitness_values_sort_sequence = np.argsort(args_pop_fitness_values)[::-1][:args_pop.shape[1]]
     best_value = args_pop_fitness_values[args_pop_fitness_values_sort_sequence[0]]
     worst_value = args_pop_fitness_values[args_pop_fitness_values_sort_sequence[len(args_pop_fitness_values_sort_sequence) - 1]]
@@ -86,9 +69,8 @@ def selection_sort(args_pop, args_pop_fitness_values):
 
     for i in range(0, args_pop.shape[1]):
         selected_i = args_pop_fitness_values_sort_sequence[i]
-        args_pop_selected[0, i] = args_pop[0, selected_i]
-        args_pop_selected[1, i] = args_pop[1, selected_i]
-        args_pop_selected[2, i] = args_pop[2, selected_i]
+        for j in range(0, args_number):
+            args_pop_selected[j, i] = args_pop[j, selected_i]
         args_pop_fitness_values_selected = np.append(args_pop_fitness_values_selected, args_pop_fitness_values[selected_i])
 
     # print(args_pop_fitness_values_selected)
@@ -96,7 +78,8 @@ def selection_sort(args_pop, args_pop_fitness_values):
 
 
 def crossover(args_pop, args_pop_selected, crossover_probability):
-    args_pop_crossovered = np.zeros((3, args_pop.shape[1]))
+    args_number = args_pop.shape[0]
+    args_pop_crossovered = np.zeros((args_number, args_pop.shape[1]))
     # crossover_point = np.uint8(1/2)
     # args_pop_selected_bits = np.empty((3, args_pop.shape[1], 8), dtype=np.uint8)
     # args_pop_selected_bits[0] = unpack_bits(args_pop_selected[0], 8)
@@ -110,15 +93,15 @@ def crossover(args_pop, args_pop_selected, crossover_probability):
         for i in range(0, args_pop.shape[1]):
             for j in range(0, args_pop.shape[1]):
                 if random_one(crossover_probability):
-                    a_i_bits = np.binary_repr(int(args_pop_selected[0, i]*100))
-                    a_j_bits = np.binary_repr(int(args_pop_selected[0, j]*100))
-                    args_pop_crossovered[0, i] = int(a_i_bits[:len(a_i_bits)-1] + a_j_bits[len(a_j_bits)-1:], 2)/100
-                    b_i_bits = np.binary_repr(int(args_pop_selected[1, i]))
-                    b_j_bits = np.binary_repr(int(args_pop_selected[1, j]))
-                    args_pop_crossovered[1, i] = int(b_i_bits[:len(b_i_bits)-1] + b_j_bits[len(b_j_bits)-1:], 2)
-                    c_i_bits = np.binary_repr(int(args_pop_selected[2, i]))
-                    c_j_bits = np.binary_repr(int(args_pop_selected[2, j]))
-                    args_pop_crossovered[2, i] = int(c_j_bits[:len(c_i_bits)-1] + c_j_bits[len(c_j_bits)-1:], 2)
+                    for k in range(0, args_number):
+                        if k == 0:
+                            a_i_bits = np.binary_repr(int(args_pop_selected[k, i]*100))
+                            a_j_bits = np.binary_repr(int(args_pop_selected[k, j]*100))
+                            args_pop_crossovered[k, i] = int(a_i_bits[:len(a_i_bits)-1] + a_j_bits[len(a_j_bits)-1:], 2)/100
+                        else:
+                            p_i_bits = np.binary_repr(int(args_pop_selected[k, i]))
+                            p_j_bits = np.binary_repr(int(args_pop_selected[k, j]))
+                            args_pop_crossovered[k, i] = int(p_i_bits[:len(p_i_bits)-1] + p_j_bits[len(p_j_bits)-1:], 2)
                     crossovered_pop_size += 1
                     break
                 if crossovered_pop_size >= args_pop.shape[1]:
@@ -130,17 +113,18 @@ def crossover(args_pop, args_pop_selected, crossover_probability):
 
 
 def mutation(args_pop, mutation_probability):
-    args_pop_mutated = np.zeros((3, args_pop.shape[1]))
+    args_number = args_pop.shape[0]
+    args_pop_mutated = np.zeros((args_number, args_pop.shape[1]))
 
     for i in range(0, args_pop.shape[1]):
-        a_i_bits = np.binary_repr(int(args_pop[0, i] * 100))
-        a_i_bits_mutated = a_i_bits[0] + mutate_bits(a_i_bits[1:], mutation_probability)
-        args_pop_mutated[0, i] = int(a_i_bits_mutated, 2) / 100
-        b_i_bits = np.binary_repr(int(args_pop[1, i]))
-        b_i_bits_mutated = mutate_bits(b_i_bits, mutation_probability)
-        args_pop_mutated[1, i] = int(b_i_bits_mutated, 2)
-        c_i_bits = np.binary_repr(int(args_pop[2, i]))
-        c_i_bits_mutated = mutate_bits(c_i_bits, mutation_probability)
-        args_pop_mutated[2, i] = int(c_i_bits_mutated, 2)
+        for k in range(0, args_number):
+            if k == 0:
+                a_i_bits = np.binary_repr(int(args_pop[k, i] * 100))
+                a_i_bits_mutated = a_i_bits[0] + mutate_bits(a_i_bits[1:], mutation_probability)
+                args_pop_mutated[k, i] = int(a_i_bits_mutated, 2) / 100
+            else:
+                p_i_bits = np.binary_repr(int(args_pop[k, i]))
+                p_i_bits_mutated = mutate_bits(p_i_bits, mutation_probability)
+                args_pop_mutated[k, i] = int(p_i_bits_mutated, 2)
 
     return args_pop_mutated
--- main.py
+++ main.py
@@ -17,7 +17,8 @@ def main():
     plt.plot(positive_points[0], positive_points[1], 'b.')
     plt.axis([-10, 20, -10, 20])
 
-    start(negative_points, positive_points)
+    find_polynomial(2, negative_points, positive_points)
+
     plt.show()
     print_title('Genetic Algorithms end')
 
@@ -48,6 +49,32 @@ def generate_and_save_points():
         pickle.dump(random_points_positive, fp)
 
 
+def find_polynomial(degree, negative_points, positive_points):
+    param_pop_size = 1000  # 2000
+    param_crossover_probability = 0.4  # 0.4
+    param_mutation_probability = 0.12  # 0.02
+    param_generation_number = 10  # 20
+
+    polynomials = {
+        2: init_polynomial_pop_degree_two(param_pop_size),
+        3: init_polynomial_pop_degree_three(param_pop_size),
+        4: init_polynomial_pop_degree_four(param_pop_size),
+        5: init_polynomial_pop_degree_five(param_pop_size)
+    }
+
+    initial_pop = polynomials.get(degree, lambda: 'Invalid Polynomial Degree')
+
+    start(
+        negative_points,
+        positive_points,
+        y_polynomial,
+        initial_pop,
+        param_crossover_probability,
+        param_mutation_probability,
+        param_generation_number
+    )
+
+
 def get_negative_points():
     with open('random_points_negative', 'rb') as fp:
         return pickle.load(fp)
@@ -56,6 +83,57 @@ def get_negative_points():
 def get_positive_points():
     with open('random_points_positive', 'rb') as fp:
         return pickle.load(fp)
+
+
+def init_polynomial_pop_degree_two(args_pop_size):
+    a_params = np.random.uniform(low=0.01, high=1, size=args_pop_size)
+    b_params = np.random.randint(low=-20, high=20, size=args_pop_size)
+    c_params = np.random.randint(low=-200, high=200, size=args_pop_size)
+    # return np.random.randint(low=1, high=10, size=(2, args_pop_size))
+    return np.concatenate(([a_params], [b_params], [c_params]))
+
+
+def init_polynomial_pop_degree_three(args_pop_size):
+    a_params = np.random.uniform(low=0.01, high=1, size=args_pop_size)
+    b_params = np.random.uniform(low=0.01, high=1, size=args_pop_size)
+    c_params = np.random.randint(low=-20, high=20, size=args_pop_size)
+    d_params = np.random.randint(low=-200, high=200, size=args_pop_size)
+    # return np.random.randint(low=1, high=10, size=(2, args_pop_size))
+    return np.concatenate(([a_params], [b_params], [c_params], [d_params]))
+
+
+def init_polynomial_pop_degree_four(args_pop_size):
+    a_params = np.random.uniform(low=0.01, high=1, size=args_pop_size)
+    b_params = np.random.uniform(low=0.01, high=1, size=args_pop_size)
+    c_params = np.random.uniform(low=0.01, high=1, size=args_pop_size)
+    d_params = np.random.randint(low=-20, high=20, size=args_pop_size)
+    e_params = np.random.randint(low=-200, high=200, size=args_pop_size)
+    # return np.random.randint(low=1, high=10, size=(2, args_pop_size))
+    return np.concatenate(([a_params], [b_params], [c_params], [d_params], [e_params]))
+
+
+def init_polynomial_pop_degree_five(args_pop_size):
+    a_params = np.random.uniform(low=0.01, high=1, size=args_pop_size)
+    b_params = np.random.uniform(low=0.01, high=1, size=args_pop_size)
+    c_params = np.random.uniform(low=0.01, high=1, size=args_pop_size)
+    d_params = np.random.uniform(low=0.01, high=1, size=args_pop_size)
+    e_params = np.random.randint(low=-20, high=20, size=args_pop_size)
+    f_params = np.random.randint(low=-200, high=200, size=args_pop_size)
+    # return np.random.randint(low=1, high=10, size=(2, args_pop_size))
+    return np.concatenate(([a_params], [b_params], [c_params], [d_params], [e_params]))
+
+
+def y_polynomial(x, *argv):
+    y = 0
+    for idx, arg in enumerate(argv):
+        if idx == 0:
+            y = arg
+        elif idx == 1:
+            y = y + (arg * x)
+        else:
+            y = y + (arg * (x ** idx))
+
+    return y
 
 
 main()
